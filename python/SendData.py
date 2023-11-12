86cdc94a52ff347489eb5b1c8a9809ffbf15f1a3
vinhee <S10221118@connect.np.edu.sg>
2023-11-12 15:09:34 +0800

    Added API KEY

--- python/SendData.py
+++ python/SendData.py
@@ -1,12 +1,21 @@
 import pyrebase
 
-config ={
-    # to put apikey of firebase    
+config = {
+  "apiKey": "AIzaSyAQDU7-hbWtI3sa3s3woi4YfUJtiVRYhGg",
+  "authDomain": "fbeye-bf2f1.firebaseapp.com",
+  "databaseURL": "https://fbeye-bf2f1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "fbeye-bf2f1",
+  "storageBucket": "fbeye-bf2f1.appspot.com",
+  "messagingSenderId": "462631567055",
+  "appId": "1:462631567055:web:bc8ee774e09e56e8fcc1ba",
+  "measurementId": "G-83NWPL2V92",
+  "databaseURL": "https://fbeye-bf2f1-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(config)
 database = firebase.database()
 
-data = {"EmployeeID": 1, "Name": "Test", "CheckinTime": "8:30:10", "CheckoutTime": null}
+data = {"EmployeeID": 1, "Name": "Test", "CheckinTime": "8:30:10", "CheckoutTime": "null"}
 
-database.child("Employees").set(data)
+database.push(data)
+#database.child("Employees").set(data)
