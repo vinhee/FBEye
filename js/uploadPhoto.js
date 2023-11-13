35ebe7ee05b8ccb08212efad7668994c669cb41f
charmaine <s10244220@connect.np.edu.sg>
2023-11-14 01:15:43 +0800

    Modify uploadPhoto.html & uploadPhoto.js

--- js/uploadPhoto.js
+++ js/uploadPhoto.js
@@ -2,6 +2,7 @@ const selectImage = document.querySelector('.select-image');
 const inputFile = document.querySelector('#file');
 const imgArea = document.querySelector('.img-area');
 
+//uploading image for show
 selectImage.addEventListener('click', function () {
   inputFile.click();
 })
@@ -25,3 +26,65 @@ inputFile.addEventListener('change', function () {
     alert("Image size more than 2MB");
   }
 })
+
+//configure firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAQDU7-hbWtI3sa3s3woi4YfUJtiVRYhGg",
+    authDomain: "fbeye-bf2f1.firebaseapp.com",
+    databaseURL: "https://fbeye-bf2f1-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fbeye-bf2f1",
+    storageBucket: "fbeye-bf2f1.appspot.com",
+    messagingSenderId: "462631567055",
+    appId: "1:462631567055:web:bc8ee774e09e56e8fcc1ba",
+  };
+
+  const app = firebase.initializeApp(firebaseConfig);
+
+  const storage = firebase.storage();
+  const getImageData = (e) => {
+    file = e.target.files[0];
+    fileName = Math.round(Math.random() * 9999) + file.name;
+    if (fileName) {
+      fileData.style.display = "block";
+    }
+    fileData.innerHTML = fileName;
+    console.log(file, fileName);
+  };
+
+  const uploadImage = () => {
+    loading.style.display = "block";
+    const storageRef = storage.ref().child("myimages");
+    const folderRef = storageRef.child(fileName);
+    const uploadtask = folderRef.put(file);
+    uploadtask.on(
+      "state_changed",
+      (snapshot) => {
+        console.log("Snapshot", snapshot.ref.name);
+        progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        progress = Math.round(progress);
+        progressbar.style.width = progress + "%";
+        progressbar.innerHTML = progress + "%";
+        uploadedFileName = snapshot.ref.name;
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref("myimages")
+          .child(uploadedFileName)
+          .getDownloadURL()
+          .then((url) => {
+            console.log("URL", url);
+            if (!url) {
+              img.style.display = "none";
+            } else {
+              img.style.display = "block";
+              loading.style.display = "none";
+            }
+            img.setAttribute("src", url);
+          });
+        console.log("File Uploaded Successfully");
+      }
+    );
+  };
