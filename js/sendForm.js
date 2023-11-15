423ed5f2da4e5238277e872c19b1693d6356c085
vinhee <S10221118@connect.np.edu.sg>
2023-11-15 11:50:32 +0800

    Generate UID for firebase push

--- js/sendForm.js
+++ js/sendForm.js
@@ -1,17 +1,4 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAQDU7-hbWtI3sa3s3woi4YfUJtiVRYhGg",
-    authDomain: "fbeye-bf2f1.firebaseapp.com",
-    databaseURL: "https://fbeye-bf2f1-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "fbeye-bf2f1",
-    storageBucket: "fbeye-bf2f1.appspot.com",
-    messagingSenderId: "462631567055",
-    appId: "1:462631567055:web:bc8ee774e09e56e8fcc1ba",
-    measurementId: "G-83NWPL2V92"
-  };
-
-firebase.initialize(firebaseConfig);
-
-var sendForm = firebase.database().ref("Request");
+/* var sendForm = firebase.database().ref("Request");
 
 document.getElementById('sendForm').addEventListener("submit", submitForm);
 
@@ -44,4 +31,4 @@ const sendRequest = (name, employeeID, startDate, endDate, MCNo, place) => {
 const getElementById = (id) => {
     return document.getElementById(id).value;
 
-}
+} */
